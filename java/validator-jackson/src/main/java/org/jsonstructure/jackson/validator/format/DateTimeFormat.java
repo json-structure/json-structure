c079345464487ff508b6d12e53457272ad264fdf
Michael Spiegel <michael.m.spiegel@gmail.com>
2017-03-18 13:44:40 -0400

    Add date-time format parsing.

--- java/validator-jackson/src/main/java/org/jsonstructure/jackson/validator/format/DateTimeFormat.java
+++ java/validator-jackson/src/main/java/org/jsonstructure/jackson/validator/format/DateTimeFormat.java
@@ -1,5 +1,9 @@
 package org.jsonstructure.jackson.validator.format;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -19,6 +23,13 @@ public class DateTimeFormat implements Format {
     public String apply(@Nonnull JsonNode value, @Nonnull String type) {
         if (!value.isTextual()) {
             return null;
+        }
+        try {
+            String str = value.textValue();
+            str = str.replace("-00:00", "+00:00");
+            ZonedDateTime.parse(str, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        } catch (DateTimeParseException ex) {
+            return "value is not a valid rfc 3339 date-time format: " + ex.getMessage();
         }
         return null;
     }
